4daa874b458f49387c673660b08f445769cf4a17
Joey Turczak <joeyturczak@gmail.com>
2022-03-27 13:18:43 -0700

    Add views

---
+++ resources/views/hardware.blade.php
@@ -0,0 +1,45 @@
+@extends('adminlte::page')
+
+@section('plugins.Datatables', true)
+
+@section('title', 'Manufacturers')
+
+@section('content_header')
+    <h1>Manufacturers</h1>
+@stop
+
+@section('content')
+<div class="card">
+  <div class="card-body">
+    <table id="table" class="table table-bordered">
+      <thead>
+        <tr>
+          <th style="width: 10px">#</th><th>Name</th><th style="width: 100px">Label</th>
+        </tr>
+      </thead>
+      <tbody>
+        
+        @foreach($manufacturers AS $manufacturer)
+        <tr>
+          <td>{{ $manufacturer->id }}</td>
+          <td>{{ $manufacturer->name }}</td>
+          <td>
+            <a class="btn btn-default btn-sm" href="{{ route('manufacturers.show', ['manufacturer'=>$manufacturer->id]) }}">View Contact Info</a>
+          </td>
+        </tr>
+        @endforeach
+
+      </tbody>
+    </table>
+  </div>
+</div>
+<a href="{{ route('manufacturers.create') }}" class="btn btn-primary">Create</a>
+@stop
+
+@section('js')
+<script>
+    $(document).ready(function() {
+        $('#table').DataTable();
+    } );
+</script>
+@stop
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,41 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view('welcome');
+});
+
+Route::get('/manufacturers', function() {
+    return view('manufacturers');
+});
+
+Route::get('/hardware', function() {
+    return view('hardware');
+});
+
+Route::get('/equipment', function() {
+    return view('equipment');
+});
+
+Route::get('/users', function() {
+    return view('users');
+});
+
+
+Route::fallback(function() {
+    return view('notfound');
+});
+
+Route::get('/db-test', function () {
+    try {         
+         echo \DB::connection()->getDatabaseName();     
+    } catch (\Exception $e) {
+          echo 'None';
+    }
+});
+
+Route::get('/db-migrate', function() {
+    Artisan::call('migrate');
+    echo Artisan::output();
+});
