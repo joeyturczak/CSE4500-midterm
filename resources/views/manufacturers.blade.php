bd6d52451811789e27195d6aa0879ed9d6cab73f
Joey Turczak <joeyturczak@gmail.com>
2022-03-27 15:08:17 -0700

    Update manufacturers.blade.php

--- resources/views/manufacturers.blade.php
+++ resources/views/manufacturers.blade.php
@@ -14,7 +14,7 @@
     <table id="table" class="table table-bordered">
       <thead>
         <tr>
-          <th style="width: 10px">#</th><th>Name</th><th style="width: 120px">Contact Info</th><th style="width: 40px">Action</th>
+          <th style="width: 10px">#</th><th>Name</th><th style="width: 100px">Contact Info</th><th style="width: 40px">Action</th>
         </tr>
       </thead>
       <tbody>
