8cd01b4f736cf1248cefb21e2344d0ba8031f541
Joey Turczak <joeyturczak@gmail.com>
2022-03-27 23:04:34 -0700

    Update deviceusers.blade.php

--- resources/views/deviceusers.blade.php
+++ resources/views/deviceusers.blade.php
@@ -14,7 +14,7 @@
     <table id="table" class="table table-bordered">
       <thead>
         <tr>
-          <th style="width: 10px">#</th><th>Name</th><th style="width: 150px">Contact Info</th>
+          <th style="width: 10px">#</th><th>First Name</th><th>Last Name</th><th style="width: 90px">Contact Info</th><th style="width: 120px">Action</th>
         </tr>
       </thead>
       <tbody>
@@ -22,9 +22,23 @@
         @foreach($deviceusers AS $deviceuser)
         <tr>
           <td>{{ $deviceuser->id }}</td>
-          <td>{{ $deviceuser->name }}</td>
+          <td>{{ $deviceuser->first_name }}</td>
+          <td>{{ $deviceuser->last_name }}</td>
           <td>
-            <a class="btn btn-default btn-sm" style="text-align: center" href="{{ route('deviceusers.show', ['deviceusers'=>$deviceuser->id]) }}">View</a>
+            <div style="text-align: center;">
+              <a class="btn btn-default" href="{{ route('deviceusers.show', ['deviceuser'=>$deviceuser->id]) }}">View</a>
+            </div>
+          </td>
+          <td>
+            <div style="display: inline">   
+                   
+              <form style="margin: 0; padding: 0" action="{{ route('deviceusers.destroy', ['deviceuser'=>$deviceuser->id]) }}" method="POST">
+                <a class="btn btn-default" href="{{ route('deviceusers.edit', ['deviceuser'=>$deviceuser->id]) }}">Edit</a>
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="btn btn-danger">Delete</button>
+              </form>
+            </div>
           </td>
         </tr>
         @endforeach
