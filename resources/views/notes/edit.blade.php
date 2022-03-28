27e34bed9c2e21804c51f720b6ad331b046229ab
Joey Turczak <joeyturczak@gmail.com>
2022-03-28 16:59:29 -0700

    Update edit.blade.php

--- resources/views/notes/edit.blade.php
+++ resources/views/notes/edit.blade.php
@@ -10,20 +10,6 @@
 <form method="post" action="{{ route('notes.update', ['note'=>$note->id]) }}">
   @csrf
   @method('PUT')
-  
-  <x-adminlte-select name="deviceuser_id" value="{{ $userdevice->deviceuser_id }}" label="User">
-    @foreach($deviceusers AS $u)
-      @if($u->id == $userdevice->deviceuser_id)
-        <option value='{{ $u->id }}' selected="selected">{{ $u->first_name." ".$u->last_name }}</option>
-      @else
-        <option value='{{ $u->id }}'>{{ $u->first_name." ".$u->last_name }}</option>
-      @endif
-    @endforeach
-  </x-adminlte-select>
-
-  <x-adminlte-input name="invoice_number" value="{{ $userdevice->invoice_number }}" label="Invoice #" />
-  <x-adminlte-input name="price" type="number" step="0.01" value="{{ $userdevice->price }}" label="Price" />
-  <x-adminlte-input name="purchase_date" type="date" value="{{ $userdevice->purchase_date }}" label="Purchase Date" />
 
   <x-adminlte-select name="userdevice_id" label="Equipment">
     @foreach($userdevices AS $d)
