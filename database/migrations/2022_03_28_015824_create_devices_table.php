6daa71707ee278d89e752068fb55988962bbfb04
Joey Turczak <joeyturczak@gmail.com>
2022-03-27 18:58:50 -0700

    Create 2022_03_28_015824_create_devices_table.php

---
+++ database/migrations/2022_03_28_015824_create_devices_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('devices', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('spec_screen_size')->nullable();
+            $table->string('spec_ram')->nullable();
+            $table->string('spec_storage')->nullable();
+            $table->foreignId('manufacturer_id')->constrained('manufacturers');
+            $table->foreignId('category_id')->constrained('categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('devices');
+    }
+};
